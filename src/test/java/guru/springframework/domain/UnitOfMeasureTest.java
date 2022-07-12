81194c538eac198b9623d067bf667a6f84f05ae7
dev_afo <aboufofana76@gmail.com>
2022-07-12 14:12:36 +0200

    Unit test and integration test

---
+++ src/test/java/guru/springframework/domain/UnitOfMeasureTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.domain;
+
+import guru.springframework.repository.UnitOfMeasureRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UnitOfMeasureTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription(){
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+
+}
